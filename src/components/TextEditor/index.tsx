184bde881ba272432b2f30c0f7e1f3f82d789fdc
mwibutsa <mflohost@gmail.com>
2021-03-21 13:49:50 +0200

    Add markdown support
    - Add a preview tab to the rich form editor
    - Add markdown support for the text

--- src/components/TextEditor/index.tsx
+++ src/components/TextEditor/index.tsx
@@ -1,21 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "../Button";
+import marked from "marked";
 import "./styles.scss";
 
 export interface TextEditorProps {
   placeholder?: string;
 }
 
+const TAB_OPTIONS = {
+  edit: "edit-tab",
+  preview: "preview-tab",
+};
+
 const TextEditor: React.FC<TextEditorProps> = () => {
+  const [markdown, setMarkdown] = useState<string>("");
+  const [markedText, setMarkedText] = useState<string>("");
+  const [activeTab, setActiveTab] = useState<string>("edit-tab");
+
+  const handleTabChange = (activeTab: string) => {
+    setActiveTab(activeTab);
+  };
+
+  const handleMarkdownChange = ({
+    target: { value },
+  }: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMarkdown(value);
+  };
+
+  useEffect(() => {
+    setMarkedText(marked(markdown));
+  }, [markdown]);
+
+  const renderMarkedText = () => {
+    if (activeTab === TAB_OPTIONS.preview) {
+      return (
+        <div
+          className="editor__preview"
+          dangerouslySetInnerHTML={{ __html: markedText }}
+        ></div>
+      );
+    }
+    return null;
+  };
+
+  const renderTextArea = () => {
+    if (activeTab === TAB_OPTIONS.edit) {
+      return (
+        <textarea
+          name="description"
+          id="description"
+          placeholder="Type here..."
+          value={markdown}
+          onChange={handleMarkdownChange}
+        ></textarea>
+      );
+    }
+  };
+
   return (
     <div className="editor-container">
       <div className="editor">
         <div className="editor__title">
           <input type="text" placeholder="Title" />
         </div>
+        <div className="editor__tabs">
+          <div
+            className={
+              activeTab === TAB_OPTIONS.edit
+                ? "editor__edit-tab editor__tabs--active"
+                : "editor__edit-tab"
+            }
+            onClick={() => handleTabChange(TAB_OPTIONS.edit)}
+          >
+            Edit
+          </div>
 
+          <div
+            className={
+              activeTab === TAB_OPTIONS.preview
+                ? "editor__preview-tab editor__tabs--active"
+                : "editor__preview-tab"
+            }
+            onClick={() => handleTabChange(TAB_OPTIONS.preview)}
+          >
+            Preview
+          </div>
+        </div>
         <div className="editor__description">
-          <textarea name="description" id="description"></textarea>
+          {renderTextArea()}
+          {renderMarkedText()}
         </div>
 
         <div className="editor__actions">
